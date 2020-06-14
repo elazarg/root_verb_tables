ad836f38e54cf12a1e64fdeaf5c06d9448cecd26
Elazar Gershuni <elazarg@gmail.com>
2020-06-14 04:03:47 +0300

    add 4 and update

--- generate.py
+++ generate.py
@@ -1,22 +1,46 @@
 import random
 
-BASEDIR = 'שלמים'
-ROOTS = BASEDIR + '/roots.txt'
-TABLE = BASEDIR + '/table.tsv'
-
-with open(ROOTS, encoding='utf8') as f:
-    roots = [line.split() for line in f.read().split('\n')]
-
-root = random.choice(roots)
-
-with open(TABLE, encoding='utf8') as f:
-    forms = f.read()
-
-d = forms.replace('.ק', root[0]).replace('.ט', root[1]).replace('.ל', root[2])
+PRE = '_'
 
 
-def replace_sofiot(txt):
-    return txt.replace('כ.', 'ך').replace('מ.', 'ם').replace('נ.', 'ן').replace('פ.', 'ף').replace('צ.', 'ץ').replace('.', '')
+def read_template(basedir):
+    ROOTS = basedir + '/roots.txt'
+    PROTO = basedir + '/proto.txt'
+    TABLE = basedir + '/table.tsv'
+
+    with open(PROTO, encoding='utf8') as f:
+        proto = f.read().strip()
+
+    with open(ROOTS, encoding='utf8') as f:
+        roots = [line.split()[::-1] for line in f.read().split('\n')]
+
+    with open(TABLE, encoding='utf8') as f:
+        templates = f.read()
+
+    return proto, roots, templates
 
 
-print(replace_sofiot(d))
+def instantiate(proto, root, templates):
+    res = templates
+    for p, c in zip(proto, root):
+        res = res.replace(PRE + p, c)
+
+    return res\
+        .replace(PRE + 'כ', 'ך')\
+        .replace(PRE + 'מ', 'ם')\
+        .replace(PRE + 'נ', 'ן')\
+        .replace(PRE + 'פ', 'ף')\
+        .replace(PRE + 'צ', 'ץ')\
+        .replace(PRE, '')
+
+
+def instantiate_random(basedir):
+    proto, roots, templates = read_template(basedir)
+    root = random.choice(roots)
+    return instantiate(proto, root, templates)
+
+
+if __name__ == '__main__':
+    print(instantiate_random('שלמים'))
+    print()
+    print(instantiate_random('מרובעים'))
