5b3093afa94772dc642ffcfd2eca4a328b74ec70
Elazar Gershuni <elazarg@gmail.com>
2020-06-19 18:00:30 +0300

    3 and 4

--- generate_table_for_root.py
+++ generate_table_for_root.py
@@ -30,9 +30,9 @@ def read_root(root, tag):
     return proto, templates
 
 
-def load_roots_map():
+def load_roots_map(numbers):
     roots_map = {}
-    for n in [3, 4]:
+    for n in numbers:
         with heb_io.open_file('roots_{}_tagged.tsv'.format(n)) as f:
             for line in f.read().strip().split('\n'):
                 w, *root, tag = line.split()
@@ -40,16 +40,20 @@ def load_roots_map():
     return roots_map
 
 
-roots_map = load_roots_map()
+roots_map_3 = load_roots_map([3])
+roots_map_4 = load_roots_map([4])
+roots_map_all = load_roots_map([3, 4])
 
-roots = list(roots_map)
+roots_3 = list(roots_map_3)
+roots_4 = list(roots_map_4)
+roots_all = list(roots_map_all)
 
 
 def read_template(w) -> str:
-    root, tag = roots_map[w]
+    root, tag = roots_map_all[w]
     proto, templates = read_root(root, tag)
     return instantiate(proto, root, templates)
 
 
 if __name__ == '__main__':
-    print(read_template(random.choice(roots)))
+    print(read_template(random.choice(roots_all)))
